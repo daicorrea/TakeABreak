a3eb4c9d33bb707597dcb3aac38185bd3c3a3687
Daiane Correa <daoli@br.ibm.com>
2018-01-19 22:02:27 -0200

    Using ctime library to show when program started and ended

--- take_break_anita.py
+++ take_break_anita.py
@@ -4,7 +4,9 @@ import webbrowser
 my_breaks = 0
 break_count = 1
 
+print('This program started on ' + time.ctime())
 while my_breaks < break_count:
     time.sleep(5)
     webbrowser.get('safari').open_new('www.youtube.com/watch?v=kDhptBT_-VI')
     my_breaks += 1
+print('This program ended on ' + time.ctime())
