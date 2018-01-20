09e2c1a565ea00fd99e63ccf70e7d1174a474b3f
Daiane Correa <daoli@br.ibm.com>
2018-01-19 22:01:39 -0200

    Adding time library to count seconds to the loop

--- take_break_anita.py
+++ take_break_anita.py
@@ -1,8 +1,10 @@
+import time
 import webbrowser
 
 my_breaks = 0
 break_count = 1
 
 while my_breaks < break_count:
+    time.sleep(5)
     webbrowser.get('safari').open_new('www.youtube.com/watch?v=kDhptBT_-VI')
     my_breaks += 1
