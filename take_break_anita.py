3f9060a9c0a73ffeccc82518aea203d684f0981b
Daiane Correa <daoli@br.ibm.com>
2018-01-19 22:00:43 -0200

    Opening a browser link on Safari

---
+++ take_break_anita.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+my_breaks = 0
+break_count = 1
+
+while my_breaks < break_count:
+    webbrowser.get('safari').open_new('www.youtube.com/watch?v=kDhptBT_-VI')
+    my_breaks += 1
